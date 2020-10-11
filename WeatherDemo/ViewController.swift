0a9647713e45f259b51b972b205df88c4d43d49a
smitesh patel <smitesh.patel1@rci.rogers.ca>
2020-10-11 15:46:11 -0400

    Initial Commit

---
+++ WeatherDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by Smitesh Patel on 2020-10-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
